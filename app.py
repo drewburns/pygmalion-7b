93c3035952cebc29693237bc020aaadfbcdbf969
Andrew Burns <drewburnsbab@gmail.com>
2023-06-01 09:52:25 -0400

    changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from transformers import GPTJForCausalLM, GPT2Tokenizer
+# from transformers import GPTJForCausalLM, GPT2Tokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
 import torch
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -10,7 +12,7 @@ def init():
     global tokenizer
 
     print("loading to CPU...")
-    model = GPTJForCausalLM.from_pretrained("PygmalionAI/pygmalion-6b", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model = AutoModelForCausalLM.from_pretrained("Neko-Institute-of-Science/pygmalion-7b")
     print("done")
 
     # conditionally load to GPU
@@ -19,7 +21,8 @@ def init():
         model.cuda()
         print("done")
 
-    tokenizer = GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
+    tokenizer = AutoTokenizer.from_pretrained("Neko-Institute-of-Science/pygmalion-7b")
+
 
 
 # Inference is ran for every server call
@@ -28,21 +31,32 @@ def inference(model_inputs:dict) -> dict:
     global model
     global tokenizer
 
+
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
+
+
+    character = "Selena, the Bewitching Serpent"
+    persona = "Selena, the Bewitching Serpent, is a fascinating creature dwelling in the heart of the enchanted forest. Her captivating voice and mesmerizing presence lure the curious into her realm. Her hypnotic gaze holds the promise of forbidden secrets and untamed desires. Yet, beneath her alluring exterior lie enigmatic motives. Engage her in conversation, and brace yourself for a dialogue that's as enticing as it is perilous. Remember - not everything that glitters is gold."
+
+    input_text = f"{character}'s Persona: {persona}\n<START>\nYou: {prompt}\n{character}:"
+
     # Tokenize inputs
-    input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
+    # input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
 
-    # Run the model
-    output = model.generate(input_tokens)
+    # # Run the model
+    # output = model.generate(input_tokens)
 
-    # Decode output tokens
-    output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
+    # # Decode output tokens
+    # output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
 
-    result = {"output": output_text}
+    # result = {"output": output_text}
 
+    # Generate
+    inputs = tokenizer(input_text, return_tensors="pt")
+    generate_ids = model.generate(inputs.input_ids, max_length=100)
+    return tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     # Return the results as a dictionary
-    return result
+    #return result
--- download.py
+++ download.py
@@ -1,22 +1,29 @@
-# In this file, we define download_model
-# It runs during container build time to get model weights built into the container
+# # In this file, we define download_model
+# # It runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface GPTJ model
+# # In this example: A Huggingface GPTJ model
 
-from transformers import GPTJForCausalLM, GPT2Tokenizer
+# from transformers import GPTJForCausalLM, GPT2Tokenizer
+# import torch
+
+# def download_model():
+#     # do a dry run of loading the huggingface model, which will download weights
+#     print("downloading model...")
+#     GPTJForCausalLM.from_pretrained(
+#         "PygmalionAI/pygmalion-6b", torch_dtype=torch.float16, low_cpu_mem_usage=True
+#     )
+#     print("done")
+
+#     print("downloading tokenizer...")
+#     GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
+#     print("done")
+
+
+from transformers import LlamaTokenizer, LlamaForCausalLM
 import torch
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    print("downloading model...")
-    GPTJForCausalLM.from_pretrained(
-        "PygmalionAI/pygmalion-6b", torch_dtype=torch.float16, low_cpu_mem_usage=True
-    )
-    print("done")
-
-    print("downloading tokenizer...")
-    GPT2Tokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
-    print("done")
-
+    model = LlamaForCausalLM.from_pretrained("Neko-Institute-of-Science/pygmalion-7b", torch_dtype=torch.float32)
+    tokenizer = LlamaTokenizer.from_pretrained("Neko-Institute-of-Science/pygmalion-7b")
 if __name__ == "__main__":
     download_model()
